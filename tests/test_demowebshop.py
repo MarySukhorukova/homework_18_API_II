68003fd9db5e21d863d52807d37020a92ceef3b2
MarySukhorukova <57411226+MarySukhorukova@users.noreply.github.com>
2023-03-10 20:50:20 +0300

    fix tests

--- tests/test_demowebshop.py
+++ tests/test_demowebshop.py
@@ -12,48 +12,48 @@ login = os.getenv("LOGIN")
 
 def test_login(auth_via_api):
 
-    auth_via_api.open("")
+    browser.open("")
 
     with step("Verify successful authorization"):
-        auth_via_api.element(".account").should(have.text(login))
+        browser.element(".account").should(have.text(login))
 
 
 def test_open_product_card(auth_via_api):
 
-    auth_via_api.open("/smartphone")
+    browser.open("/smartphone")
 
     with step("Verify cart product open"):
-        auth_via_api.element('input.product-box-add-to-cart-button').click()
-        auth_via_api.element('div.product-name').should(be.visible)
+        browser.element('input.product-box-add-to-cart-button').click()
+        browser.element('div.product-name').should(be.visible)
 
 
 def test_successful_search(auth_via_api):
 
-    auth_via_api.open("")
-    auth_via_api.element('[value="Search store"]').type('14.1-inch Laptop').press_enter()
+    browser.open("")
+    browser.element('[value="Search store"]').type('14.1-inch Laptop').press_enter()
 
     with step("Verify product found"):
-        auth_via_api.element('.product-title').should(have.text('14.1-inch Laptop'))
+        browser.element('.product-title').should(have.text('14.1-inch Laptop'))
 
 
 def test_unsuccessful_search(auth_via_api):
 
-    auth_via_api.open("")
-    auth_via_api.element('[value="Search store"]').type('Lapptop').press_enter()
+    browser.open("")
+    browser.element('[value="Search store"]').type('Lapptop').press_enter()
 
     with step("Verify product found"):
-        auth_via_api.element('.search-results').should(have.text('No products were found that matched your criteria.'))
+        browser.element('.search-results').should(have.text('No products were found that matched your criteria.'))
 
 
 def test_add_product_to_cart(auth_via_api):
 
-    auth_via_api.open("/computing-and-internet")
-    auth_via_api.element('input#add-to-cart-button-13').click()
-    auth_via_api.element('li#topcartlink > a').click()
-    auth_via_api.driver.refresh()
+    browser.open("/computing-and-internet")
+    browser.element('input#add-to-cart-button-13').click()
+    browser.element('li#topcartlink > a').click()
+    browser.driver.refresh()
 
-    auth_via_api.element('td.product > a').should(have.text('Computing and Internet'))
+    browser.element('td.product > a').should(have.text('Computing and Internet'))
 
-    auth_via_api.element('td.remove-from-cart > input').click()
-    auth_via_api.element('input.update-cart-button').click()
-    auth_via_api.element('.order-summary-content').should(have.text('Your Shopping Cart is empty!'))
+    browser.element('td.remove-from-cart > input').click()
+    browser.element('input.update-cart-button').click()
+    browser.element('.order-summary-content').should(have.text('Your Shopping Cart is empty!'))
